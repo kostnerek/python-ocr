4fff45dd207f69e00c5dd3f991d354d15ac3144e
Filip Kostecki <filip.kostecki@gate-software.com>
2020-08-19 15:33:22 +0200

    button press

--- ocr.py
+++ ocr.py
@@ -1,8 +1,12 @@
 import requests
 import time
+import ctypes
 
 import pyautogui
 import os
+
+
+
 
 def ocr_space_file(filename, overlay=False, api_key='fc0d1ddf2788957', language='eng'):
     payload = {'isOverlayRequired': overlay,
@@ -31,14 +35,31 @@ def parseImage(filePath):
 
     return parsedText
 
+def DetectClick(button, watchtime = 5):
+    '''Waits watchtime seconds. Returns True on click, False otherwise'''
+    if button in (1, '1', 'l', 'L', 'left', 'Left', 'LEFT'):
+        bnum = 0x01
+    elif button in (2, '2', 'r', 'R', 'right', 'Right', 'RIGHT'):
+        bnum = 0x02
+
+    start = time.time()
+    while 1:
+        if ctypes.windll.user32.GetKeyState(bnum) not in [0, 1]:
+            # ^ this returns either 0 or 1 when button is not being held down
+            print(pyautogui.position())
+            return True
+        elif time.time() - start >= watchtime:
+            break
+        time.sleep(0.001)
+    return False
 
 filePath = os.getcwd()+"\\screen.jpg"
 
-for x in range(99):
+i=0
+while i>=0:
+    DetectClick('left')
+
+""" for x in range(99):
     print(parseImage(filePath))
-    time.sleep(1)
-
-
-
-
+    time.sleep(1) """
 
